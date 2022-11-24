66b53eff9040adb487a09b8d7a91ec91385a7156
ErickMarchetti <erickgmarchetti@gmail.com>
2022-11-23 21:43:48 -0300

    feat: comando para criacao de tipos automaticamente

---
+++ shops/management/commands/create_types.py
@@ -0,0 +1,27 @@
+from shops.models import Type
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help: str = "Cria tipos prontos na model type"
+
+    types = [
+        {"descricao": "Débito", "sinal": "+"},
+        {"descricao": "Boleto", "sinal": "-"},
+        {"descricao": "Financiamento", "sinal": "-"},
+        {"descricao": "Crédito", "sinal": "+"},
+        {"descricao": "Recebimento Empréstimo", "sinal": "+"},
+        {"descricao": "Vendas", "sinal": "+"},
+        {"descricao": "Recebimento TED", "sinal": "+"},
+        {"descricao": "Recebimento DOC", "sinal": "+"},
+        {"descricao": "Aluguel", "sinal": "-"},
+    ]
+
+    def handle(self, *args, **options):
+        for type in self.types:
+            type["natureza"] = "Entrada" if type["sinal"] == "+" else "Saída"
+
+            new_type = Type(**type)
+            new_type.full_clean()
+
+            new_type.save()
