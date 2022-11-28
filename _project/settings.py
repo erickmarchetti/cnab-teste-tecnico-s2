d3f19a4d1bdb5311f526f692a81dca8b523d0e2d
ErickMarchetti <erickgmarchetti@gmail.com>
2022-11-27 21:39:12 -0300

    feat: layout da pagina

--- _project/settings.py
+++ _project/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = "_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -125,7 +125,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
